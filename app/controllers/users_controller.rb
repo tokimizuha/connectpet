f7791926d4cf337047e4f55264645e05adeaf991
tokimizuha <dearl.g.s.lacie0722@yahoo.ne.jp>
2022-10-04 23:39:16 +0900

    user info更新アクション実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,16 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :emali, :profile, :profile_image)
+  end
+  
 end
