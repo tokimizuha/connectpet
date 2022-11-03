e0b89c82bef8149864402c39878dcbfc4c96a17b
tokimizuha <dearl.g.s.lacie0722@yahoo.ne.jp>
2022-11-03 22:15:13 +0900

    新規登録管理

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -9,6 +9,19 @@ class PetsController < ApplicationController
     @pet = Pet.new
   end
 
+  def create
+    @pet = Pet.new(pet_params)
+    @pet.user_id = current_user.id
+    @pet.save
+    redirect_to pet_path(@pet)
+  end
+
   def edit
   end
+
+  private
+  def pet_params
+    params.require(:pet).permit(:title, :body, :image)
+  end
+
 end
