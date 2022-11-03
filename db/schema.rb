d717dae0fb7b9070840b9505ea117f976f0599e7
tokimizuha <dearl.g.s.lacie0722@yahoo.ne.jp>
2022-11-03 20:06:37 +0900

    pet table作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_02_115958) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_03_110208) do
+  create_table "pets", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "title"
+    t.text "body"
+    t.string "image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
