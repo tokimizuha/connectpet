67a5eb86ba16e3c8f22b790179f0abcd17d93406
tokimizuha <dearl.g.s.lacie0722@yahoo.ne.jp>
2022-11-03 20:11:08 +0900

    pets routing設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'pets/index'
-  get 'pets/show'
-  get 'pets/new'
-  get 'pets/edit'
   devise_for :users
   root to: "home#index"
   resources :users
+  resources :pets
 end
