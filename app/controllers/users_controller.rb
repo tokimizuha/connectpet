f4d6030ddefcbff4e15f75013cbf0eb1e60181f0
tokimizuha <dearl.g.s.lacie0722@yahoo.ne.jp>
2022-11-09 00:32:55 +0900

    emali→emailに修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:username, :emali, :profile, :profile_image)
+    params.require(:user).permit(:username, :email, :profile, :profile_image)
   end
   
 end
