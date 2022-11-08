37b68ee292f399a7b1be7db60972771baf56b6d6
tokimizuha <dearl.g.s.lacie0722@yahoo.ne.jp>
2022-11-09 00:30:27 +0900

    sign_out メソッドをgetに設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   private
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
   end
